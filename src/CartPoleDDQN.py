20de166c196eb2ea7eadab14acdf21b3cb5b6001
gianvr <gvgiancarlo1@gmail.com>
2024-04-07 20:57:37 -0300

    cartpole

--- src/CartPoleDDQN.py
+++ src/CartPoleDDQN.py
@@ -13,51 +13,52 @@ from algorithms.DoubleDQN import DoubleDQN
 
 print(f'{os.environ["KERAS_BACKEND"]}')
 
-env = gym.make('CartPole-v1')
-#env.seed(0)
-np.random.seed(0)
+for i in range(5):
+    env = gym.make('CartPole-v1')
+    #env.seed(0)
+    np.random.seed(0)
 
-print('State space: ', env.observation_space)
-print('Action space: ', env.action_space)
+    print('State space: ', env.observation_space)
+    print('Action space: ', env.action_space)
 
-model = keras.Sequential()
-model.add(keras.layers.Dense(512, activation="relu", input_dim=env.observation_space.shape[0]))
-model.add(keras.layers.Dense(256, activation="relu"))
-model.add(keras.layers.Dense(128, activation="relu"))
-model.add(keras.layers.Dense(env.action_space.n, activation="linear"))
-model.summary()
+    model = keras.Sequential()
+    model.add(keras.layers.Dense(512, activation="relu", input_dim=env.observation_space.shape[0]))
+    model.add(keras.layers.Dense(256, activation="relu"))
+    model.add(keras.layers.Dense(128, activation="relu"))
+    model.add(keras.layers.Dense(env.action_space.n, activation="linear"))
+    model.summary()
 
 
-gamma = 0.99
-epsilon = 1.0 
-epsilon_min = 0.01
-epsilon_dec = 0.95
-learning_rate = 0.001
-episodes = 200
-batch_size = 64
-memory = deque(maxlen=10000)
-max_steps = 1500
-interval = 2
+    gamma = 0.99
+    epsilon = 1.0 
+    epsilon_min = 0.01
+    epsilon_dec = 0.99
+    learning_rate = 0.001
+    episodes = 300
+    batch_size = 64
+    memory = deque(maxlen=10000)
+    max_steps = 1500
+    interval = 2
 
-model.compile(loss=keras.losses.MeanSquaredError(), optimizer=keras.optimizers.Adam(learning_rate=learning_rate))
+    model.compile(loss=keras.losses.MeanSquaredError(), optimizer=keras.optimizers.Adam(learning_rate=learning_rate))
 
-DDQN = DoubleDQN(env, gamma, epsilon, epsilon_min, epsilon_dec, episodes, batch_size, memory, model, max_steps, interval)
-rewards = DDQN.train()
+    DDQN = DoubleDQN(env, gamma, epsilon, epsilon_min, epsilon_dec, episodes, batch_size, memory, model, max_steps, interval)
+    rewards = DDQN.train()
 
-import matplotlib.pyplot as plt
-plt.plot(rewards)
-plt.xlabel('Episodes')
-plt.ylabel('# Rewards')
-plt.title('# Rewards vs Episodes')
-plt.savefig("results/cart_pole_DDQN.jpg")     
-plt.close()
+    import matplotlib.pyplot as plt
+    plt.plot(rewards)
+    plt.xlabel('Episodes')
+    plt.ylabel('# Rewards')
+    plt.title('# Rewards vs Episodes')
+    plt.savefig(f"results/cart_pole_DDQN_{i+1}.jpg")     
+    plt.close()
 
-with open('results/cart_pole_DDQN_rewards.csv', 'a', newline='') as file:
-    writer = csv.writer(file)
-    episode=0
-    for reward in rewards:
-        writer.writerow([episode,reward])
-        episode+=1
+    with open(f'results/cart_pole_DDQN_rewards_{i+1}.csv', 'a', newline='') as file:
+        writer = csv.writer(file)
+        episode=0
+        for reward in rewards:
+            writer.writerow([episode,reward])
+            episode+=1
 
-model.save('data/model_DDQN_cart_pole.keras')
+    model.save(f'data/model_DDQN_cart_pole_{i+1}.keras')
 
